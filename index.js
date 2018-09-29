ffd6659a76b1ac123515474dcc9ddb8ff2aeba98
nicholasyyu <nicholas.yu24@gmail.com>
2018-09-29 00:34:58 +0000

    done

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  if (fromN === toN) {
+    return fromN;
+  }
 
+  let result = fromN + 1;
+
+  if(result <= toN){
+  	return fromN + sum(result, toN);
+  }
+
+}
+console.log(sum(4,8));
 module.exports = sum;
